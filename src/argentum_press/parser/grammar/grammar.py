7fbe861755a00993ce5b97ca9e52a2cb8157ef36
Kristian Nymann Jakobsen <kristian@nymann.dev>
2026-05-14 19:22:27 +0200

    parser: handle '@p98 (Behold the Sinister Six!)
    {
      "parent_rule": "cardexpression",
      "alternative_text": "possessiveterm? (valueterm | thatmanyexpression | \"a\")? \"card\"[\"s\"] \"with\" \"different\" \"names\" (\"of\" zonedeclarationexpression)?",
      "label": "cardexpressionwithdifferentnames"
    }

--- src/argentum_press/parser/grammar/grammar.py
+++ src/argentum_press/parser/grammar/grammar.py
@@ -856,7 +856,7 @@ def getGrammar():
         ABILITYMODIFIER: "triggered" | "activated" | "mana" | "loyalty"
         COMBATSTATUSMODIFIER: "attacking" | "defending" | "attacked" | "blocking" | "blocked" | "active"
         KEYWORDSTATUSMODIFIER: "paired" | "kicked" | "face-up" | "face-down" | "transformed" | "enchanted" | "equipped"
-        | "fortified" | "monstrous" | "regenerated" | "suspended" | "flipped" | "suspected" | "modified" // TODO: ensure 'suspected' works properly
+        | "fortified" | "monstrous" | "regenerated" | "suspended" | "flipped" | "suspected" | "modified" | "different" // TODO: ensure 'suspected' works properly
         TAPPEDSTATUSMODIFIER: "tapped" | "untapped"
         EFFECTSTATUSMODIFIER: "named" | "chosen" | "chosen at random" | "revealed" | "returned" | "destroyed" | "exiled" | "died" | "countered" | "sacrificed"
         | "the target of a spell or ability" | "prevented" | "created" | "cast"
@@ -884,11 +884,12 @@ def getGrammar():
         PLAYERCHARACTERISTIC: "maximum hand size" | "life total"["s"] | "life" | "cards in hand"
         # OBJECTCHARACTERISTIC: "card"? "name" | "mana cost" | "converted mana cost" | "color"["s"] | "color indicator" | "type"["s"] | "card type"["s"] | "subtype"["s"] | "supertype"["s"]
         # | "rules text" | "abilities" | "power" | "toughness" | "base power" | "base toughness" | "loyalty" | "hand modifier" | "life modifier"
-        OBJECTCHARACTERISTIC: "card"? "name" | "mana value" | "color"["s"] | "color indicator" | "type"["s"] | "card type"["s"] | "subtype"["s"] | "supertype"["s"]
+        OBJECTCHARACTERISTIC: "card"? "name"["s"] | "mana value" | "color"["s"] | "color indicator" | "type"["s"] | "card type"["s"] | "subtype"["s"] | "supertype"["s"]
         | "rules text" | "abilities" | "power" | "toughness" | "base power" | "base toughness" | "loyalty" | "hand modifier" | "life modifier"
 
         //[TODO: Not quite done, there are expressions like 'a number of cards equal to [...]'. There's some overlapping responsibilities with descriptions involving cards, maybe.]
         !cardexpression: possessiveterm? ("the" "top" | "the" "bottom")? (valueterm | thatmanyexpression | "a")? ("or" valueterm)? "card"["s"] ("from" "the" "top" | "from" "the" "bottom")? ("of" zonedeclarationexpression)?
+        | possessiveterm? (valueterm | thatmanyexpression | "a")? "card"["s"] "with" "different" "names" ("of" zonedeclarationexpression)? -> cardexpressionwithdifferentnames
 
         zonedeclarationexpression: (declarationdecorator* | referencedecorator*) zone
         | "the" ("top" | "bottom") "of" zonedeclarationexpression -> topbottomofzonedecl
