f8f67e6fd67a368ec94b8b1e2533db83d698ffcb
Kristian Nymann Jakobsen <kristian@nymann.dev>
2026-05-15 04:07:09 +0200

    parser: handle 'm'@p52 (Shadow of the Goblin)
    {
      "parent_rule": "manaspendablestatement",
      "alternative_text": "\"at\" \"the\" \"beginning\" \"of\" \"your\" \"first\" \"main\" \"phase\" \",\" \"discard\" \"a\" \"card\"",
      "label": "firstmainphasediscardstatement"
    }

--- src/argentum_press/parser/grammar/grammar.py
+++ src/argentum_press/parser/grammar/grammar.py
@@ -41,6 +41,7 @@ def getGrammar():
         activationrestrictionstatement: "activate" "only" "as" "a" "sorcery"
         manaspendablestatement: "mana" "of" "any" "type" "can" "be" "spent" "to" "cast" "spells" "this" "way"
         | "you" "do" "not" "lose" "unspent" "red" "mana" "as" "steps" "and" "phases" "end" -> manaretentionstatement
+        | "at" "the" "beginning" "of" "your" "first" "main" "phase" "," "discard" "a" "card" -> firstmainphasediscardstatement
         triggerrestrictionstatement: declarationorreference? "trigger"["s"] "only" valuefrequency timeexpression?
 
         thenstatement: "then" statement
